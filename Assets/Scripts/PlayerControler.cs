bcc41ce3ec439ee07992ee78042b80756a3f69c0
czw <lchu976@gmail.com>
2020-10-23 18:53:39 -0400

    Platform Runner
    First version

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    //"Public" variables: SerializeField - 目的为了别人看不到但是自己可以在Unity里面改
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 500.0f;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+    //private variable
+    private Rigidbody2D rBody;
+    private Animator anim;
+    private bool isGrounded = false;
+    private bool isFacingRight = true;
+    private bool isDead;
+
+    public GameObject restartText;
+    public GameObject winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    //Physics
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+
+        if( isDead == true)
+        {
+            restartText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+
+            return;
+        }
+        
+        //jump code. 
+        /*一般在 &&情况下无所谓顺序，但是 isGrounded 是一个 bool 类型的数据。放前面对于运算更简便
+         所以在这里顺序很重要*/
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rBody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        //Check is the sprite needs to be flipped
+        if((isFacingRight && rBody.velocity.x < 0) || (!isFacingRight && rBody.velocity.x>0))
+        {
+            Flip();
+        }
+        
+        //Commuicate with the animator
+        anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("yVelocity", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+
+    //转身
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "deedly")
+        {
+            anim.SetBool("die", true);
+            rBody.velocity = Vector2.zero;
+            isDead = true;
+        }
+
+        if (other.tag == "Win")
+        {
+            winText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/VirtualCameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class VirtualCameraController : MonoBehaviour
+{
+    public List<GameObject> VirtualCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualCamera.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            VirtualCamera.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+
+    public void TransitionTo(GameObject cameraToTransitionTo)
+    {
+        foreach (GameObject g in VirtualCamera)
+        {
+            if (g == cameraToTransitionTo)
+            {
+                //Transition to that camera
+                g.GetComponent<CinemachineVirtualCamera>().Priority = 10;
+            }
+            else
+            {
+                //De-priority camera
+                g.GetComponent<CinemachineVirtualCamera>().Priority = 5;
+            }
+        }
+    }
+}
